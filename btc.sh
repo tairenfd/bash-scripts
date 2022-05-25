e54e40251fe0feabd6f6be754c5bb7b72523e158
Tairen Dunham <tairenfd@mailbox.org>
2022-05-25 10:53:09 -0400

    1btc script, status updated with btc tracker

---
+++ btc.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+curl -s http://api.coindesk.com/v1/bpi/currentprice.json |
+python -c "import json, sys; 
+print(json.load(sys.stdin)['bpi']['USD']['rate'])"
--- status.sh
+++ status.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 while true;
 do
+  btc=$(btc.sh)
   mem=$(memfree.sh) 
   blue=$(bluetooth.sh)
   time=$(date '+%a %b %d | %H:%M ')
-  xsetroot -name "$blue | Free: $mem | $time"
+  xsetroot -name "$blue| BTC: \$$btc | Free: $mem | $time"
   sleep 5;
 done;
